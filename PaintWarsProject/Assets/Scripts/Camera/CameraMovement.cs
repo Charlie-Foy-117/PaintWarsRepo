a24c25c5980bd92d7499ca1aa63e9469782ee754
Charlie-Foy-117 <charliefoy03@gmail.com>
2022-05-20 15:55:51 +0100

    Added Cam Movement and Started Walking Anim

---
+++ PaintWarsProject/Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    //declares object to be used as a target
+    public Transform player;
+    //sets delay when moving
+    public float delayTime;
+    //declares position variables that will be changed for cam
+    public Vector3 cameraPosition;
+    public Vector3 velocity = Vector3.zero;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //sets the target location for camera to move to
+        cameraPosition = new Vector3(player.position.x, 0, -10f);
+        //moves the camera if its not at the players location
+        transform.position = Vector3.SmoothDamp(gameObject.transform.position, cameraPosition, ref velocity, delayTime);
+    }
+}
