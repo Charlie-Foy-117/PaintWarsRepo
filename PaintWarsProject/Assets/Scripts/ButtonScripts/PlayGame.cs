0a01863879b1582ff13f1673a292967d588214b5
Charlie-Foy-117 <charliefoy03@gmail.com>
2022-05-31 16:21:26 +0100

    Created a menu play button

---
+++ PaintWarsProject/Assets/Scripts/ButtonScripts/PlayGame.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI; //required when referring to the button component
+using UnityEngine.EventSystems; //required when using event data
+
+
+[RequireComponent(typeof(Button))]
+public class PlayGame : MonoBehaviour
+{
+    //well store out button here so we can use it
+    private Button button;
+
+    //this fuction is called when the script first loads, before any start methods
+    private void Awake()
+    {
+        //when the object first starts
+        button = GetComponent<Button>();
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+}
